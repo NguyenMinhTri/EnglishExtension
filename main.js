9861ead59888f4462a5f50b8d4ba251d665c4da4
NguyenMinhTri <nmtri.uit@gmail.com>
2017-12-30 18:20:02 +0700

    init

---
+++ main.js
@@ -0,0 +1,47 @@
+var apiKey = "trnsl.1.1.20171031T094741Z.b8618875ef69a6bb.d4baa2b7efd346de3d5d51c9d805ab3cc18aada9";
+var langDirection = "en-zh"
+
+// Add bubble to the top of the page.
+var bubble = document.createElement('div');
+bubble.setAttribute('class', 'bubble');
+document.body.appendChild(bubble);
+
+// Lets listen to mouseup DOM events.
+document.addEventListener('mouseup', function (e) {
+  var selection = window.getSelection().toString();
+  if (selection.length > 0) {
+    translateText(e.clientX-20, e.clientY+30, selection);
+  }
+}, false);
+
+// Close the bubble when we click on the screen.
+document.addEventListener('mousedown', function (e) {
+  bubble.style.visibility = 'hidden';
+}, false);
+
+function translateText(mouseX, mouseY, selection) {
+  var url = "https://uitenglishbot.herokuapp.com/dictionary?voca="  + selection
+  fetch(url)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+        if (/^[a-z\d\-_\s]+$/i.test(data.messages[1].text)) {
+          renderBubble(mouseX, mouseY, "Translation not found.");
+        } else {
+          new Audio(data.messages[3].attachment.payload.url).play();
+          renderBubble(mouseX, mouseY, data.messages[1].text);
+        }
+    })
+    .catch(function(err) {
+        console.error('An error ocurred', err);
+    });
+}
+
+// Move that bubble to the appropriate location.
+function renderBubble(mouseX, mouseY, selection) {
+  bubble.innerHTML = selection;
+  bubble.style.top = mouseY + 'px';
+  bubble.style.left = mouseX + 'px';
+  bubble.style.visibility = 'visible';
+}
