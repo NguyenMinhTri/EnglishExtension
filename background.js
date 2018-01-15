c2159553859978a8b2af7805d87f6ef94f5c8688
NguyenMinhTri <nmtri.uit@gmail.com>
2018-01-15 21:11:54 +0700

    update

--- background.js
+++ background.js
@@ -1,144 +1,196 @@
 
 //
+chrome.cookies.set({
+    url: 'https://uitenglishbot.herokuapp.com/',
+    name: "duplicate",
+    value: "temp"
+});
+var tempTime = Date.now();
 var urlSound = "https://translate.googleapis.com/translate_tts?client=gtx&ie=UTF-8&tl=vi&tk=775040.775040&q=";
-        chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
-            var result = "";
-            if (request.name == "soundSetting") {
-                chrome.cookies.get({
-                    url: 'https://uitenglishbot.herokuapp.com/',
-                    name: 'sound_setting'
-                }, function(cookie) {
-                    if (cookie === null || cookie == null) {
-                        sendResponse("ON");
-                    } else {
-                        sendResponse(cookie.value);
-                    }
-                });
-
-            } else if (request.name == "runAudio") {
-                new Audio(request.audio).play();
-                console.log(sender);
-                console.log(request);
-                sendResponse(request.audio);
-            } else if (request.name == "getAllCookies") {
-                chrome.cookies.getAll({ url: "http://olympusenglish.azurewebsites.net" }, function(cookies) {
-                    var tempCookieValue = '';
-                    for (var i = 0; i < cookies.length; i++) {
-                        tempCookieValue += cookies[i].name + '=' + cookies[i].value + '; ';
-                    }
-                    sendResponse(tempCookieValue);
-                });
-            } else if(request.name == "getDictToExtension") {
-                
-                var url = "http://olympusenglish.azurewebsites.net/Dictionary/getDictToExtension?contain=" + request.audio;
-                fetch(url, {
-                        credentials: 'include'
-                    })
-                    .then(function(response) {
-                        return response.json();
-                    })
-                    .then(function(data) {
-                            sendResponse(data);
-                        //}
-                    })
-                    .catch(function(err) {
-                        console.log('Có lỗi xảy ra!', err);
-                    });
-
-            } else if(request.name == "verifyToken"){
-                var url = "http://olympusenglish.azurewebsites.net/Dictionary/verifyToken";
-                fetch(url, {
-                        credentials: 'include'
-                    })
-                    .then(function(response) {
-                        return response.json();
-                    })
-                    .then(function(data) {
-                            sendResponse(data);
-                        //}
-                    })
-                    .catch(function(err) {
-                        console.log('Có lỗi xảy ra!', err);
-                    });
+var urlSoundEn = "https://translate.googleapis.com/translate_tts?client=gtx&ie=UTF-8&tl=en&tk=775040.775040&q=";
+function playVoca(url) {
+    return new Promise(function (resolve, reject) { // return a promise
+        if(tempTime < ( Date.now() - 300))
+        {
+            var audio = new Audio();                     // create audio wo/ src
+            audio.preload = "auto";                      // intend to play through
+            audio.autoplay = true;                       // autoplay when loaded
+            audio.onerror = reject;                      // on error, reject
+            audio.onended = resolve;                     // when done, resolve
+            audio.src = url
+            tempTime = Date.now();
+        }
+    });
+}
+chrome.extension.onRequest.addListener(function (request, sender, sendResponse) {
+    var result = "";
+    if (request.name == "soundSetting") {
+        chrome.cookies.get({
+            url: 'https://uitenglishbot.herokuapp.com/',
+            name: 'sound_setting'
+        }, function (cookie) {
+            if (cookie === null || cookie == null) {
+                sendResponse("ON");
+            } else {
+                sendResponse(cookie.value);
             }
-
         });
-        document.addEventListener('DOMContentLoaded', function() {
-           // var urlSound = "https://translate.googleapis.com/translate_tts?client=gtx&ie=UTF-8&tl=vi&tk=775040.775040&q="
-            // chrome.tabs.onUpdated.addListener(checkForValidUrl);
-            //  new Audio("https://translate.googleapis.com/translate_tts?client=gtx&ie=UTF-8&tl=vi&tk=775040.775040&q=Tr%E1%BB%9Di%20m%C3%A1").play();
-            //chrome.pageAction.onClicked.addListener(onClickListener); //might need to put this here, it's been a while since I've done a chrome extension, but if you do then just put your conditional for the regex in your onClickListener function
-            chrome.cookies.getAll({ url: "http://olympusenglish.azurewebsites.net" }, function(cookies) {
-                var tempCookieValue = '';
-                var checkLoading = false;
-                chrome.cookies.set({
-                    url: 'https://uitenglishbot.herokuapp.com/',
-                    name: '.AspNet.ApplicationCookie',
-                    value: ''
-                });
-                for (var i = 0; i < cookies.length; i++) {
-                    tempCookieValue += cookies[i].name + '=' + cookies[i].value + '; ';
-                    chrome.cookies.set({
-                        url: 'https://uitenglishbot.herokuapp.com/',
-                        name: cookies[i].name,
-                        value: cookies[i].value,
-                        expirationDate: cookies[i].expirationDate
-                    });
-                    if (cookies[i].name == '.AspNet.ApplicationCookie') {
-                        checkLoading = true;
-                    }
-                }
-                if (checkLoading == false) {
-                    new Audio(urlSound + "Hình như bạn chưa đăng nhập vào Olympus").play();
-                } else {
-                    fetch("https://uitenglishbot.herokuapp.com/verifyToken", {
-                            credentials: 'include'
-                        })
-                        .then(function(response) {
-                            try {
-                                return response.json();
-                            } catch (err) {
-                                return null;
-                            }
-                        })
-                        .then(function(data) {
-                            if (data.text === null || data.text == null) {
-                                new Audio(urlSound + "Hình như bạn chưa đăng nhập vào Olympus").play();
-                            } else {
-                                new Audio(urlSound + "Xin chào bạn" + data.text).play();
-                            }
-                        });
-                }
 
+    } else if (request.name == "runAudio") {
+        playVoca(request.audio).then(function () {
+            sendResponse(Date.now());
+        });
+    }
+    else if (request.name == "runGoogleAudio") {
+        playVoca(urlSoundEn + request.audio).then(function () {
+            sendResponse(Date.now());
+        });
+    } else if (request.name == "getAllCookies") {
+        chrome.cookies.getAll({ url: "http://olympusenglish.azurewebsites.net" }, function (cookies) {
+            var tempCookieValue = '';
+            for (var i = 0; i < cookies.length; i++) {
+                tempCookieValue += cookies[i].name + '=' + cookies[i].value + '; ';
+            }
+            sendResponse(tempCookieValue);
+        });
+    } else if(request.name == "determineDuplicate") {
+        chrome.cookies.get({
+            url: 'https://uitenglishbot.herokuapp.com/',
+            name: 'duplicate'
+        }, function (cookie) {
+            if (cookie === null || cookie == null) {
+                sendResponse("");
+            } else {
+                sendResponse(cookie.value);
+            }
+        });
+    } else if(request.name == "setCookieDuplicate") {
+        chrome.cookies.set({
+            url: 'https://uitenglishbot.herokuapp.com/',
+            name: "duplicate",
+            value: request.audio
+        });
+    }  
+    
+    else if (request.name == "getDictToExtension") {
+
+        var url = "http://olympusenglish.azurewebsites.net/Dictionary/getDictToExtension?contain=" + request.audio;
+        fetch(url, {
+            credentials: 'include'
+        })
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                sendResponse(data);
+                //}
+            })
+            .catch(function (err) {
+                console.log('Có lỗi xảy ra!', err);
             });
 
+    } else if (request.name == "verifyToken") {
+        var url = "http://olympusenglish.azurewebsites.net/Dictionary/verifyToken";
+        fetch(url, {
+            credentials: 'include'
+        })
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                sendResponse(data);
+                //}
+            })
+            .catch(function (err) {
+                console.log('Có lỗi xảy ra!', err);
+            });
+    }
+
+});
+document.addEventListener('DOMContentLoaded', function () {
+    // var urlSound = "https://translate.googleapis.com/translate_tts?client=gtx&ie=UTF-8&tl=vi&tk=775040.775040&q="
+    // chrome.tabs.onUpdated.addListener(checkForValidUrl);
+    //  new Audio("https://translate.googleapis.com/translate_tts?client=gtx&ie=UTF-8&tl=vi&tk=775040.775040&q=Tr%E1%BB%9Di%20m%C3%A1").play();
+    //chrome.pageAction.onClicked.addListener(onClickListener); //might need to put this here, it's been a while since I've done a chrome extension, but if you do then just put your conditional for the regex in your onClickListener function
+    chrome.cookies.getAll({ url: "http://olympusenglish.azurewebsites.net" }, function (cookies) {
+        var tempCookieValue = '';
+        var checkLoading = false;
+        chrome.cookies.set({
+            url: 'https://uitenglishbot.herokuapp.com/',
+            name: '.AspNet.ApplicationCookie',
+            value: ''
         });
-
-        // Called when the url of a tab changes.
-        function checkForValidUrl(tabId, changeInfo, tab) {
-            // Show action only for wikipedia pages
-            // var regex = /wikipedia.org/gi;
-            // if (tab.url.match(regex)) {
-            chrome.pageAction.show(tabId);
-            chrome.pageAction.onClicked.addListener(onClickListener);
-            // }
-        };
-
-        function onClickListener(tab) {
-            alert('Clicked!!!');
+        for (var i = 0; i < cookies.length; i++) {
+            tempCookieValue += cookies[i].name + '=' + cookies[i].value + '; ';
+            chrome.cookies.set({
+                url: 'https://uitenglishbot.herokuapp.com/',
+                name: cookies[i].name,
+                value: cookies[i].value,
+                expirationDate: cookies[i].expirationDate
+            });
+            if (cookies[i].name == '.AspNet.ApplicationCookie') {
+                checkLoading = true;
+            }
+        }
+        if (checkLoading == false) {
+           // new Audio(urlSound + "Hình như bạn chưa đăng nhập vào Olympus").play();
+        } else {
+            fetch("https://uitenglishbot.herokuapp.com/verifyToken", {
+                credentials: 'include'
+            })
+                .then(function (response) {
+                    try {
+                        return response.json();
+                    } catch (err) {
+                        return null;
+                    }
+                })
+                .then(function (data) {
+                    if (data.text === null || data.text == null) {
+                        //    new Audio(urlSound + "Hình như bạn chưa đăng nhập vào Olympus").play();
+                    } else {
+                        //   new Audio(urlSound + "Xin chào bạn" + data.text).play();
+                    }
+                });
         }
 
+    });
+
+});
+
+// Called when the url of a tab changes.
+function checkForValidUrl(tabId, changeInfo, tab) {
+    // Show action only for wikipedia pages
+    // var regex = /wikipedia.org/gi;
+    // if (tab.url.match(regex)) {
+    chrome.pageAction.show(tabId);
+    chrome.pageAction.onClicked.addListener(onClickListener);
+    // }
+};
+
+function onClickListener(tab) {
+    alert('Clicked!!!');
+}
 
 
-    
+
+
 $.connection.hub.url = "http://olympusenglish.azurewebsites.net/signalR";
-
+//$.connection.hub.url = "http://localhost:20000/signalR";
 var notification = $.connection.notificationHub;
 
 $.connection.hub.start().
-done(function () { console.log('Now connected, connection ID='); })
-.fail(function () { console.log('Could not connect'); });
+    done(function () { console.log('Now connected, connection ID='); })
+    .fail(function () { console.log('Could not connect'); });
 
 notification.client.receiveMessage = function (msg) {
-    new Audio(urlSound + msg).play();
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, msg, function (response) { 
+         });
+    });
+};
+
+notification.client.updateUsersOnlineCount = function (count) {
+    chrome.browserAction.setBadgeBackgroundColor({ color: [255, 0, 0, 255] });
+    chrome.browserAction.setBadgeText({text: count.toString()});
 };
--- main.js
+++ main.js
@@ -6,7 +6,7 @@ bubble.setAttribute('class', 'bubble');
 document.body.appendChild(bubble);
 
 // Lets listen to mouseup DOM events.
-document.addEventListener('mouseup', function(e) {
+document.addEventListener('mouseup', function (e) {
 	var target = e.target.closest(".bubble button");
 	if (target == null) {
 		var selection = window.getSelection().toString();
@@ -17,7 +17,7 @@ document.addEventListener('mouseup', function(e) {
 }, false);
 
 // Close the bubble when we click on the screen.
-document.addEventListener('mousedown', function(e) {
+document.addEventListener('mousedown', function (e) {
 	var target = e.target.closest("div.bubble");
 	if (target == null) {
 		bubble.style.visibility = "hidden";
@@ -31,11 +31,11 @@ document.addEventListener('mousedown', function(e) {
 }, false);
 
 function translateText(mouseX, mouseY, selection) {
-	chrome.extension.sendRequest({ name: "getAllCookies", audio: "" }, function(responseAllCookies) {
-		chrome.extension.sendRequest({ name: "getDictToExtension", audio: selection }, function(data) {
-			chrome.extension.sendRequest({ name: "soundSetting", audio: "" }, function(soudSetting) {
+	chrome.extension.sendRequest({ name: "getAllCookies", audio: "" }, function (responseAllCookies) {
+		chrome.extension.sendRequest({ name: "getDictToExtension", audio: selection }, function (data) {
+			chrome.extension.sendRequest({ name: "soundSetting", audio: "" }, function (soudSetting) {
 				if (soudSetting === 'ON') {
-					chrome.extension.sendRequest({ name: "runAudio", audio: data.messages[3].attachment.payload.url }, function(response) {
+					chrome.extension.sendRequest({ name: "runAudio", audio: data.messages[3].attachment.payload.url }, function (response) {
 						console.log(response);
 					});
 				}
@@ -60,25 +60,26 @@ function translateText(mouseX, mouseY, selection) {
 function renderBubble(mouseX, mouseY, selection, voca, audio) {
 	var url = "https://uitenglishbot.herokuapp.com/saveword?voca=" + voca;
 	var saveword = "fetch(" + '"' + url + '"' + "," + "{credentials:'include',Cookie:" + '"' + allCookies + '"' + "}" + ").then(function(response) {return response.json();});"
-    //var voice = "new Audio('"+audio+"').play()";
-    var voice = "chrome.extension.sendRequest({ name: '" + "runAudio" + "', audio: '" + audio + "'}, function(response) {});";
-    if (!bubble.classList.contains('hidden')) {
-    	bubble.innerHTML = selection;
-    	bubble.style.top = mouseY + 'px';
-    	bubble.style.left = mouseX + 'px';
-    	bubble.style.visibility = 'visible';
-    	eval("" +
-    		"document.getElementsByClassName('saveWord')[0].addEventListener('click',function(event){" +
-    		saveword +
-    		"if (document.getElementsByClassName('saveWord')[0].classList.contains('selected')){this.classList.remove('selected')}else{this.classList.add('selected')}" +
-    		"});" +
-    		"document.getElementsByClassName('voice')[0].addEventListener('click',function(event){" +
-    		voice +
-    		"});"
-    		);
-    } else {
-    	bubble.classList.remove('hidden');
-    }
+	//var voice = "new Audio('"+audio+"').play()";
+	var voice = "chrome.extension.sendRequest({ name: '" + "runAudio" + "', audio: '" + audio + "'}, function(response) {});";
+	if (!bubble.classList.contains('hidden')) {
+
+		bubble.innerHTML = selection;
+		bubble.style.top = mouseY + 'px';
+		bubble.style.left = mouseX + 'px';
+		bubble.style.visibility = 'visible';
+		eval("" +
+			"document.getElementsByClassName('saveWord')[0].addEventListener('click',function(event){" +
+			saveword +
+			"if (document.getElementsByClassName('saveWord')[0].classList.contains('selected')){this.classList.remove('selected')}else{this.classList.add('selected')}" +
+			"});" +
+			"document.getElementsByClassName('voice')[0].addEventListener('click',function(event){" +
+			voice +
+			"});"
+		);
+	} else {
+		bubble.classList.remove('hidden');
+	}
 }
 
 function jsUcfirst(string) {
@@ -92,13 +93,105 @@ function jsUcfirst(string) {
 function removeLastChar(string) {
 	if (string != null) {
 		var check = string.match(/, /gi);
-		if (check != null && check.length > 1){
+		if (check != null && check.length > 1) {
 			return string.substring(0, string.length - 2);
 		}
-		else{
+		else {
 			return string;
 		}
 	} else {
 		return "";
 	}
 }
+
+//
+chrome.runtime.onMessage.addListener(function (data, sender, sendResponse) {
+		data = JSON.parse(data);
+		if (typeof data.strVoca !== "undefined") {
+			console.log(data);
+			chrome.extension.sendRequest({ name: "runAudio", audio: data.messages[3].attachment.payload.url }, function (response) {
+				console.log(response);
+				chrome.extension.sendRequest({ name: "runGoogleAudio", audio: data.messages[2].text }, function (response) {
+					console.log(response);
+				});
+			});
+			iziToast.show({
+				title: data.strVoca,
+				message: data.messages[2].text
+			});
+		}
+		else if (typeof data.Id !== "undefined") {
+			chrome.extension.sendRequest({ name: "runAudio", audio: data.SoundUrl }, function (response) {
+				console.log(response);
+				chrome.extension.sendRequest({ name: "runGoogleAudio", audio: data.MeanEn }, function (response) {
+					console.log(response);
+				});
+			});
+			iziToast.show({
+				timeout: 10000,
+				close: false,
+				theme: 'dark',
+				icon: '',
+				title: data.VocaID + " " + data.Pron,
+				message: data.MeanVi + "<br/>" + data.MeanEn,
+				position: 'bottomRight', // bottomRight, bottomLeft, topRight, topLeft, topCenter, bottomCenter
+				progressBarColor: 'rgb(0, 255, 184)',
+				buttons: [
+					['<button>Ok</button>', function (instance, toast) {
+						//openInNewTab(data.urlQuestion);
+					}, true], // true to focus
+					['<button>Close</button>', function (instance, toast) {
+						instance.hide({
+							transitionOut: 'fadeOutUp',
+							onClosing: function (instance, toast, closedBy) {
+								console.info('closedBy: ' + closedBy); // The return will be: 'closedBy: buttonName'
+							}
+						}, toast, 'buttonName');
+					}]
+				],
+				onOpening: function (instance, toast) {
+					console.info('callback abriu!');
+				},
+				onClosing: function (instance, toast, closedBy) {
+					console.info('closedBy: ' + closedBy); // tells if it was closed by 'drag' or 'button'
+				}
+			});
+		}else if (typeof data.text !== "undefined") {
+			iziToast.show({
+				timeout: 8000,
+				close: false,
+				theme: 'dark',
+				icon: '',
+				title: "Bạn có một câu hỏi: ",
+				message: data.text,
+				position: 'bottomLeft', // bottomRight, bottomLeft, topRight, topLeft, topCenter, bottomCenter
+				progressBarColor: 'rgb(0, 255, 184)',
+				buttons: [
+					['<button>Ok</button>', function (instance, toast) {
+						openInNewTab(data.urlQuestion);
+					}, true], // true to focus
+					['<button>Close</button>', function (instance, toast) {
+						instance.hide({
+							transitionOut: 'fadeOutUp',
+							onClosing: function (instance, toast, closedBy) {
+								console.info('closedBy: ' + closedBy); // The return will be: 'closedBy: buttonName'
+							}
+						}, toast, 'buttonName');
+					}]
+				],
+				onOpening: function (instance, toast) {
+					console.info('callback abriu!');
+				},
+				onClosing: function (instance, toast, closedBy) {
+					console.info('closedBy: ' + closedBy); // tells if it was closed by 'drag' or 'button'
+				}
+			});
+		}
+	sendResponse("ok");
+	return true;
+});
+
+function openInNewTab(url) {
+	var win = window.open(url, '_blank');
+	win.focus();
+  }
